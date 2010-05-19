fc03ded8eeee8f19d3a69a55272e15e693368c1e
Mathieu Pillard <m.pillard@liberation.fr>
2010-05-19 16:38:19 +0200

    Translations & strings update

--- mptt_comments/management.py
+++ mptt_comments/management.py
@@ -7,15 +7,15 @@ if "notification" in settings.INSTALLED_APPS:
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
         # Notifications of stuff I'm doing
-        notification.create_notice_type("comment_posted", _("Comment Posted"), _("you have posted a comment"), default=1)
-        notification.create_notice_type("comment_replied", _("Comment Replied"), _("you have replied to a comment"), default=1)
+        notification.create_notice_type("comment_posted", _("Comment Posted"), _("You have posted a comment"), default=1)
+        notification.create_notice_type("comment_replied", _("Comment Replied"), _("You have replied to a comment"), default=1)
         
         # Notifications of stuff that concerns me directly
-        notification.create_notice_type("comment_reply_received", _("Reply To Comment Received"), _("you have received a reply to a comment"), default=2)
+        notification.create_notice_type("comment_reply_received", _("Reply To Comment Received"), _("You have received a reply to a comment"), default=2)
 
         # Notifications of stuff from my friends/users I'm following
-        notification.create_notice_type("comment_friend_posted", _("Friend Posted Comment"), _("a friend has posted a comment"), default=1)
-        notification.create_notice_type("comment_friend_replied", _("Friend Replied To Comment"), _("a friend has replied to a comment"), default=1)
+        notification.create_notice_type("comment_friend_posted", _("Friend Posted Comment"), _("A friend has posted a comment"), default=1)
+        notification.create_notice_type("comment_friend_replied", _("Friend Replied To Comment"), _("A friend has replied to a comment"), default=1)
 
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
