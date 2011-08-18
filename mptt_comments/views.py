be6f1c323000b67b4e9d2513fbd1860767961213
Mathieu Pillard <m.pillard@liberation.fr>
2011-08-18 15:19:57 +0200

    Factorize fulltree code by calling subtree with right parameters: since we are starting from the top, with include_self=True we have everything up to CUTOFF.

--- mptt_comments/views.py
+++ mptt_comments/views.py
@@ -341,21 +341,7 @@ def comments_fulltree(request, tree_id, *args, **kwargs):
         comment = comments[0]
     except IndexError:
         raise Http404("No top level comment found for tree id %s" % tree_id)
-    target = comment.content_object
-    model = target.__class__
-    template_list = [
-        "comments/%s_%s_tree.html" % tuple(str(model._meta).split(".")),
-        "comments/%s_tree.html" % model._meta.app_label,
-        "comments/tree.html"
-    ]
-    return render_to_response(
-        template_list, {
-            "object" : target,
-            "detail_comment": comment,
-            "comments": comments,
-        }, 
-        RequestContext(request, {})
-    )    
+    return comments_subtree(request, comment.pk, include_self=True, include_ancestors=True, *args, **kwargs)
     
 def comments_subtree(request, from_comment_pk, include_self=None, include_ancestors=None, *args, **kwargs):
     
