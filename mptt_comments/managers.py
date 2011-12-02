fdb02471f75f3917d67937f15f8b1aa938083d43
Mathieu Pillard <m.pillard@liberation.fr>
2011-12-02 16:28:42 +0100

    Move managers to managers.py, and make our filter_hidden_comments method also work on querysets

---
+++ mptt_comments/managers.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.contrib.comments.managers import CommentManager
+from django.db import models
+
+class CommentQuerySet(models.query.QuerySet):
+   
+    def filter_hidden_comments(self):
+        """
+        Match django's templatetags/comments.py behavior and hide is_public=False
+        comments, and is_removed=True comments if COMMENTS_HIDE_REMOVED is True.
+         
+        We need it because some views (comments_more, comments_subtree...) play 
+        with the queryset themselves instead of just using the templatetag  
+        """
+        # FIXME: We need to do something clever for those hidden comments in order
+        #        not to break the displayed tree
+        rval = self
+        field_names = [f.name for f in self.model._meta.fields]
+        if 'is_public' in field_names:
+            rval = rval.filter(is_public=True)
+        if getattr(settings, 'COMMENTS_HIDE_REMOVED', True) and 'is_removed' in field_names:
+            rval = rval.filter(is_removed=False)
+        return rval   
+
+class MpttCommentManager(CommentManager):
+    def get_query_set(self):
+        return CommentQuerySet(self.model).select_related('user')
+        
+    def filter_hidden_comments(self):
+        return super(MpttCommentManager, self).get_query_set().filter_hidden_comments()
+
--- mptt_comments/models.py
+++ mptt_comments/models.py
@@ -1,35 +1,11 @@
 import datetime
 
 from django.db import models
-from django.conf import settings
 from django.contrib.comments.models import Comment
-from django.contrib.comments.managers import CommentManager
 from django.core import urlresolvers
 
 from mptt.models import MPTTModel
-
-class MpttCommentManager(CommentManager):
-
-    def get_query_set(self):
-        return super(MpttCommentManager, self).get_query_set().select_related('user')
-    
-    def filter_hidden_comments(self):
-        """
-        Match django's templatetags/comments.py behavior and hide is_public=False
-        comments, and is_removed=True comments if COMMENTS_HIDE_REMOVED is True.
-         
-        We need it because some views (comments_more, comments_subtree...) play 
-        with the queryset themselves instead of just using the templatetag  
-        """
-        # FIXME: We need to do something clever for those hidden comments in order
-        #        not to break the displayed tree
-        rval = self
-        field_names = [f.name for f in self.model._meta.fields]
-        if 'is_public' in field_names:
-            rval = rval.filter(is_public=True)
-        if getattr(settings, 'COMMENTS_HIDE_REMOVED', True) and 'is_removed' in field_names:
-            rval = rval.filter(is_removed=False)
-        return rval
+from mptt_comments.managers import MpttCommentManager
 
 class AbstractMpttComment(MPTTModel, Comment):
 
